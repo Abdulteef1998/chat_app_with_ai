66d1c5221ac215608d5ec0a21a2139bf28a7486d
AbdulteefRashid <abdulte97@gmail.com>
2025-04-07 04:56:08 +0300

    build chatPage base

--- lib/utils/app_theme.dart
+++ lib/utils/app_theme.dart
@@ -3,5 +3,15 @@ import 'package:flutter/material.dart';
 class AppTheme {
   static ThemeData get themeData => ThemeData(
     colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    inputDecorationTheme: InputDecorationTheme(
+      border: OutlineInputBorder(borderRadius: BorderRadius.circular(16)),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(16),
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(16),
+      ),
+      errorBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(16)),
+    ),
   );
 }
--- lib/views/pages/chat_page.dart
+++ lib/views/pages/chat_page.dart
@@ -10,23 +10,100 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
+  late final TextEditingController _messageController;
   @override
   void initState() {
     super.initState();
     BlocProvider.of<ChatCubit>(context).startChattingSession();
+    _messageController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _messageController.dispose();
+    super.dispose();
   }
 
   Widget build(BuildContext context) {
     final chatCubit = BlocProvider.of<ChatCubit>(context);
     return Scaffold(
       appBar: AppBar(title: const Text('Chat With AI'), centerTitle: true),
-      body: Center(
-        child: TextButton(
-          onPressed: () async {
-            final prompt = 'Write a story about a magic backpack.';
-            await chatCubit.sendMessage(prompt);
-          },
-          child: const Text('Chat With AI'),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            children: [
+              Expanded(
+                child: BlocBuilder<ChatCubit, ChatState>(
+                  bloc: chatCubit,
+                  buildWhen: (previous, current) => current is ChatSuccess,
+                  builder: (context, state) {
+                    if (state is ChatSuccess) {
+                      final messages = state.messages;
+                      return ListView.builder(
+                        itemBuilder: (_, index) {
+                          final message = messages[index];
+                          return ListTile(
+                            title: Text(message.text),
+                            subtitle: Text(message.time.toString()),
+                            trailing:
+                                message.isUser
+                                    ? const Icon(Icons.person)
+                                    : const Icon(Icons.computer),
+                          );
+                        },
+                        itemCount: messages.length,
+                      );
+                    } else {
+                      return const Center(child: SizedBox.shrink());
+                    }
+                  },
+                ),
+              ),
+              const SizedBox(height: 24),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: _messageController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(16),
+                        ),
+                        hintText: 'Type a message',
+                      ),
+                      onSubmitted: (value) {},
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  BlocConsumer<ChatCubit, ChatState>(
+                    bloc: chatCubit,
+                    listenWhen:
+                        (previous, current) => current is SendingMessageFailed,
+                    listener: (context, state) {
+                      if (state is SendingMessageFailed) {
+                        ScaffoldMessenger.of(
+                          context,
+                        ).showSnackBar(SnackBar(content: Text(state.error)));
+                      }
+                    },
+                    builder: (context, state) {
+                      if (state is SendingMessage) {
+                        return const CircularProgressIndicator.adaptive();
+                      }
+                      return IconButton(
+                        onPressed: () {
+                          chatCubit.sendMessage(_messageController.text);
+                          _messageController.clear();
+                        },
+                        icon: Icon(Icons.send),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
